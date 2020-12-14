528296cb6f18471e51f9a24f4981638c359c3ddd
Matheus Penchel <matheus.penchel@gmail.com>
2020-12-14 11:22:44 -0300

    Added some comments to better explain how to use includes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def index
+    # Para consertar o problema de N + 1 queries, use includes e envie como argumento um símbolo com o nome
+    # da associação! Se um Book belongs_to Author, vai ser Book.all.includes(:author).. Singular
+    # Mas indo pelo Author, o Author has_many Book, então seria Author.all.includes(:books).. Plural
+
+    # Você pode ir encadeando os includes, seja por fazer
+    # includes(...).includes(...)
+    # ou fazendo
+    # includes(some_association: :some_other_association)
     @books = Book.all.includes(:author)
   end
 end
